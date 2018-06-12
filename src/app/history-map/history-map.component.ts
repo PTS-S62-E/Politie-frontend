22c856e111780404ae2cff43676ce721a1ec0cca
bvkeersop <bart.van.keersop@gmail.com>
2018-06-12 15:45:59 +0200

    date validation

--- src/app/history-map/history-map.component.ts
+++ src/app/history-map/history-map.component.ts
@@ -28,13 +28,32 @@ export class HistoryMapComponent implements OnInit {
 
   public getHistory() {
 
-    this.translocationService.getTranslocations(
-      this.licensePlate,
-      this.createValidDate(this.startDate),
-      this.createValidDate(this.endDate));
+    this.warning = '';
+
+    if (this.compareDates(this.convertToDate(this.startDate),
+      this.convertToDate(this.startDate))) {
+      this.translocationService.getTranslocations(
+        this.licensePlate,
+        this.createValidDate(this.startDate),
+        this.createValidDate(this.endDate));
+    }
+    else {
+      this.warning = 'end date should be bigger than start date.'
+    }
   }
 
-  private createValidDate(date: String) {
+  private compareDates(start: Date, end: Date): Boolean {
+    if (end < start) {
+      return false;
+    }
+    return true;
+  }
+
+  private convertToDate(date: String): Date {
+    return new Date("13-01-2011".replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+  }
+
+  private createValidDate(date: String): String {
     var validDate = date + 'T00:00:00.000Z';
     validDate = validDate.replace(/ /g, "%20");
     validDate = validDate.replace(/:/g, "%3A");
