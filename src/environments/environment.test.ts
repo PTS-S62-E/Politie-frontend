7ec6ff31e1a7998eee646b4326a8f8e12ad8fd2f
bvkeersop <bart.van.keersop@gmail.com>
2018-06-18 15:13:00 +0200

    set url

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   envName: 'test',
-  baseUrl: 'http://192.168.24.100:8082/Backend-Politie/api',
+  baseUrl: 'http://192.168.24.100:8082/Politie-Backend/api',
   administrationUrl: 'http://192.168.24.100:8080/Rekening-Administratie/api',
   trackingQueueChannel: '/exchange/REKENINGRIJDEN_EXCHANGE/fi.registration.trackingUpdate'
 };
